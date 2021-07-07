3992d95a02ac875ef72079b67ae6d9db5e9d3ac8
zzz <2105116021@qq.com>
2021-07-07 22:11:14 +0800

    second

--- src/main/java/com/z/Application.java
+++ src/main/java/com/z/Application.java
@@ -3,5 +3,7 @@ package com.z;
 public class Application {
     public static void main(String[] args) {
         System.out.println("1111");
+        System.out.println("222");
+        System.out.println("3333");
     }
 }
